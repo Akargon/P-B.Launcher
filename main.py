63bd0302c77fab086e43682afee9338d94e1b1a8
Akargon <4kargon@gmail.com>
2024-12-30 19:44:53 -0300

    Prerelease 1.1

--- main.py
+++ main.py
@@ -7,11 +7,27 @@ from threading import Thread
 import urllib.request
 import os, shutil
 import zipfile
-import uuid
-variablesurl = "https://www.dropbox.com/scl/fi/xw9igl5r3x3vkm8ihfzog/variables.txt?rlkey=c7yfxyvojnlgmekyv12mlakip&dl=1"
-urllib.request.urlretrieve(variablesurl, 'variables.txt')
+import psutil
 
-with open('variables.txt') as file:
+# Get Minecraft directory
+default_minecraft_directory = minecraft_launcher_lib.utils.get_minecraft_directory()
+launcherFolder = default_minecraft_directory + 'Pibes'
+vanillaFolder = launcherFolder + '\\vanilla'
+modpackFolder = launcherFolder + '\\modpack'
+modpackFile = modpackFolder + '\\modpack.zip'
+modsPath = modpackFolder + "\\mods"
+java = modpackFolder + "\\runtime\\bin\\java.exe"
+variables_file = launcherFolder + '\\variables.txt'
+settings_file = launcherFolder + '\\launcher_settings.txt' 
+availableRAM = psutil.virtual_memory().total
+latest_version = minecraft_launcher_lib.utils.get_latest_version()["release"]
+versionlist = [version['id'] for version in minecraft_launcher_lib.utils.get_version_list() if version['type'] == "release"]
+
+if not os.path.exists(launcherFolder):
+    os.makedirs(launcherFolder)
+variablesurl = "https://www.dropbox.com/scl/fi/xw9igl5r3x3vkm8ihfzog/variables.txt?rlkey=c7yfxyvojnlgmekyv12mlakip&dl=1"
+urllib.request.urlretrieve(variablesurl, variables_file)
+with open(variables_file) as file:
     lines = file.readlines()
     modpackurl = lines[0].strip()
     modLoader = lines[1].strip()
@@ -19,61 +35,45 @@ with open('variables.txt') as file:
     vanillaModpackVersion = lines[3].strip()
     fabricVersion = lines[4].strip()
 
-# Get latest version
-latest_version = minecraft_launcher_lib.utils.get_latest_version()["release"]
-
-# Get Minecraft directory
-default_minecraft_directory = minecraft_launcher_lib.utils.get_minecraft_directory()
-modpackFolder = default_minecraft_directory + 'Pibes'
-modpackFile = modpackFolder + '\\modpack.zip'
-modsPath = modpackFolder + "\\mods"
-java = modpackFolder + "\\runtime\\bin\\java.exe"
-versionlist = [version['id'] for version in minecraft_launcher_lib.utils.get_version_list() if version['type'] == "release"]
-settings_file = 'launcher_settings.txt' 
-
 def save_settings(): 
     with open(settings_file, 'w') as file: 
         file.write(f"{usernameinput.get()}\n") 
         file.write(f"{versioncombobox.get()}\n") 
+        file.write(f"{maxRamSlide.get()}\n") 
+        file.write(f"{seeInstalledVersionsVar.get()}\n") 
 def load_settings(): 
     if os.path.exists(settings_file): 
         with open(settings_file, 'r') as file: 
             lines = file.readlines() 
-            if len(lines) >= 2: 
-                usernameinput.insert(0, lines[0].strip()) 
-                versioncombobox.set(lines[1].strip()) 
+            usernameinput.insert(0, lines[0].strip()) 
+            versioncombobox.set(lines[1].strip())
+            maxRamSlide.set(lines[2].strip())
+            seeInstalledVersionsVar.set(lines[3].strip())
 
-
-def maximum(max_value, value):
-    max_value[0] = value
-    
+   
 def updatemc(version, directory):
     minecraft_launcher_lib.install.install_minecraft_version(version, directory) 
-    tkinter.messagebox.showinfo("Update completed", f"Minecraft {version} has been successfully updated.")
 
-def install_minecraft(version, directory, callback, progress_window):
-    try:
-        subprocess.run(minecraft_launcher_lib.install.install_minecraft_version(version, directory, callback=callback))
-    finally:
-        progress_window.destroy()
-
-def playmc(version, directory, user, xmx=8, xms=4):
+def playmc(version, directory, user, xmx):
     options = {
-    'username': user,  # Default username
-    'uuid': '',  # Placeholder UUID
-    'token': '',  # Placeholder token
-    'jvmArguments': [f'-Xmx{xmx}G',  #Ram max memory
-                     f'-Xms{xms}G',  #Ram min memory
-                     '-Dminecraft.api.auth.host=https://nope.invalid', 
-                     '-Dminecraft.api.account.host=https://nope.invalid', 
-                     '-Dminecraft.api.session.host=https://nope.invalid', 
-                     '-Dminecraft.api.services.host=https://nope.invalid'],
-    "launcherName": "P-beLauncher", # The name of your launcher
-    "launcherVersion": "1.0", # The version of your launcher
+        'username': user,  # Default username
+        'uuid': 'offline-uuid',  # Placeholder UUID
+        'token': 'offline-token',  # Placeholder token
+        'jvmArguments': [f'-Xmx{xmx}m',  # Ram max memory
+                         '-Dminecraft.api.auth.host=https://nope.invalid',
+                         '-Dminecraft.api.account.host=https://nope.invalid',
+                         '-Dminecraft.api.session.host=https://nope.invalid',
+                         '-Dminecraft.api.services.host=https://nope.invalid'],
     }
-    subprocess.run(minecraft_launcher_lib.command.get_minecraft_command(version, directory, options))
-
-
+    installedVersionList = [version['id'] for version in minecraft_launcher_lib.utils.get_installed_versions(vanillaFolder)]
+    if version in installedVersionList :
+        print(f'Running minecraft {version}')
+        subprocess.run(minecraft_launcher_lib.command.get_minecraft_command(version, directory, options))
+    else :
+        print(f'Minecraft {version} not installed, downloading now')
+        updatemc(version, directory)
+        subprocess.run(minecraft_launcher_lib.command.get_minecraft_command(version, directory, options))
+            
 def installFabric():
     fabric = modpackFolder + "\\fabric.jar"
     try:
@@ -111,39 +111,49 @@ def modpackdownload(popupmodpack) :
 def modpackdownloadpopup():   
     popupmodpack = tkinter.Toplevel()
     popupmodpack.title("Download Modpack")
-    popupmodpack.geometry('400x200')
+    popupmodpack.geometry('300x100')
     popupmodpack.resizable(1,1)
-    popupmodpacklabel = tkinter.Label(
-    popupmodpack, 
-    text="You Sure?")
+    popupmodpacklabel = tkinter.Label(popupmodpack, text="You Sure?")
     popupmodpacklabel.pack(pady=10)
     
     button_frame = tk.Frame(popupmodpack) 
-    button_frame.pack(pady=10, fill='x')
+    button_frame.pack(pady=10)
+    cancel_button = tkinter.Button(button_frame, text="Cancel", command=lambda: popupmodpack.destroy())
+    cancel_button.pack(side='left', padx=10)
     download_button = tkinter.Button(button_frame, text="Download", command=lambda: modpackdownload(popupmodpack))
     download_button.pack(side='left', padx=10)
-    cancel_button = tkinter.Button(button_frame, text="Cancel", command=lambda: popupmodpack.destroy())
-    cancel_button.pack(side='right', padx=10)
 
-def modpackplay(user, xmx=8, xms=4) :
+
+def modpackplay(user, xmx) :
     options = {
-    'username': user,  # Default username
-    'uuid': 'offline-uuid',  # Placeholder UUID
-    'token': 'offline-token',  # Placeholder token
-    'jvmArguments': [f'-Xmx{xmx}G',  #Ram max memory
-                 f'-Xms{xms}G',  #Ram min memory
-                 '-Dminecraft.api.auth.host=https://nope.invalid', 
-                 '-Dminecraft.api.account.host=https://nope.invalid', 
-                 '-Dminecraft.api.session.host=https://nope.invalid', 
-                 '-Dminecraft.api.services.host=https://nope.invalid'],
+        'username': user,  # Default username
+        'uuid': 'offline-uuid',  # Placeholder UUID
+        'token': 'offline-token',  # Placeholder token
+        'jvmArguments': [f'-Xmx{xmx}m',  # Ram max memory
+                         '-Dminecraft.api.auth.host=https://nope.invalid',
+                         '-Dminecraft.api.account.host=https://nope.invalid',
+                         '-Dminecraft.api.session.host=https://nope.invalid',
+                         '-Dminecraft.api.services.host=https://nope.invalid'],
     }
     subprocess.run(minecraft_launcher_lib.command.get_minecraft_command(modpackVersion, modpackFolder, options))
-    
 
-root = tkinter.Tk()
+def update_versions(*args): 
+    if seeInstalledVersionsVar.get(): 
+        installedVersionList = [version['id'] for version in minecraft_launcher_lib.utils.get_installed_versions(vanillaFolder)] 
+        versioncombobox['values'] = installedVersionList 
+    else: 
+        versioncombobox['values'] = versionlist 
+        versioncombobox.set(latest_version)
+
+
+
+root = tk.Tk()
 root.title("PibeLauncher")
-root.geometry('500x500')
+root.geometry('500x200')
 root.resizable(1,1)
+
+icon_image = tk.PhotoImage(file='assets\\icon.png')
+root.iconphoto(False, icon_image)
 
 frame = ttk.Frame(root)
 frame.pack(pady = 10)
@@ -152,29 +162,40 @@ username_label.pack(side='left', padx=10)
 usernameinput = tkinter.Entry(frame)
 usernameinput.pack(side='left', padx=10)
 
+seeInstalledVersionsVar = tkinter.BooleanVar()
+seeInstalledVersionsVar.set(True)
+seeInstalledVersions = tkinter.Checkbutton(frame, text='Installed Versions', variable=seeInstalledVersionsVar)
+seeInstalledVersions.pack(side='right', padx=10)
+seeInstalledVersionsVar.trace_add('write', update_versions)
+
 versioncombobox = ttk.Combobox(frame, width=17)
-versioncombobox['values'] = versionlist
 versioncombobox.set(latest_version)
 versioncombobox.pack(side='right', padx=10)
 version_label = tkinter.Label(frame, text="Version:")
 version_label.pack(side='right', padx=10)
 
+#ramFrame = ttk.Frame(root)
+#ramFrame.pack(pady=10)
+#ramLabel = tkinter.Label(ramFrame, text="Max RAM:")
+#ramLabel.pack(side='left', padx=10)
+maxRamSlide = tk.Scale(root, from_=2024, to=availableRAM / (1024 ** 2), orient='horizontal', length=200, showvalue=True, label='Max RAM')
+maxRamSlide.pack(pady=10)
 
-vanilla_frame = ttk.Frame(root)
-vanilla_frame.pack(pady=10)
-updatebutton = tkinter.Button(vanilla_frame, text="Update", command=lambda:updatemc(versioncombobox.get(), default_minecraft_directory))
-updatebutton.pack(side='left', padx=10)
-playbutton = tkinter.Button(vanilla_frame, text="Play", command=lambda:playmc(versioncombobox.get(), default_minecraft_directory, usernameinput.get()))
-playbutton.pack(side='right', padx=10)
 
-modpack_frame = ttk.Frame(root)
-modpack_frame.pack(pady=10)
-ModpackInstallButton =tk.Button(modpack_frame, text="Modpack Install", command=lambda:modpackdownloadpopup())
+buttonsFrame = ttk.Frame(root)
+buttonsFrame.pack(pady=10)
+#updatebutton = tkinter.Button(buttonsFrame, text="Update", command=lambda:updatemc(versioncombobox.get(), vanillaFolder))
+#updatebutton.pack(side='left', padx=10)
+
+ModpackInstallButton =tk.Button(buttonsFrame, text="Modpack Install", command=lambda:modpackdownloadpopup())
 ModpackInstallButton.pack(side='left', padx=10)
-ModpackPlayButton =tk.Button(modpack_frame, text="Modpack Play", command=lambda:modpackplay(usernameinput.get()))
-ModpackPlayButton.pack(side='right', padx=10)
+ModpackPlayButton =tk.Button(buttonsFrame, text="Modpack Play", command=lambda:modpackplay(usernameinput.get(), maxRamSlide.get()))
+ModpackPlayButton.pack(side='left', padx=10)
+playbutton = tkinter.Button(buttonsFrame, text="Play", command=lambda:playmc(versioncombobox.get(), vanillaFolder, usernameinput.get(), maxRamSlide.get()))
+playbutton.pack(side='left', padx=10)
 
 root.protocol("WM_DELETE_WINDOW", lambda: (save_settings(), root.destroy()))
 load_settings()
+update_versions()
 root.mainloop()
 
